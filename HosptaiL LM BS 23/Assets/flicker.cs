539b120d3ba40f91f905157419f6a69d3eb161c8
Dan Kelly <dankelly989@gmail.com>
2018-01-04 16:33:36 +1300

    Upload some files from new version

---
+++ HosptaiL LM BS 23/Assets/flicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flicker : MonoBehaviour {
+    public float minSpeed;
+    public float maxSpeed;
+    private Light light;
+    public GameObject lightFitting;
+    private MeshRenderer mesh;
+    // Use this for initialization
+    void Start () {
+        light = GetComponent<Light>();
+        mesh = lightFitting.GetComponent<MeshRenderer>();
+        StartCoroutine(Timer());
+	}
+	
+	IEnumerator Timer()
+    {
+        var al1 = Color.black;
+        var al2 = mesh.materials[0].GetColor("_Color");
+        var em1 = Color.black;
+        var em2 = mesh.materials[0].GetColor("_EmissionColor");
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpeed, maxSpeed));
+            light.enabled = !light.enabled;
+            mesh.materials[0].SetColor("_Color",al1);
+            mesh.materials[0].SetColor("_EmissionColor", em1);
+            al1 = al2;
+            em1 = em2;
+            al2 = mesh.materials[0].GetColor("_Color");
+            em2 = mesh.materials[0].GetColor("_EmissionColor");
+        }
+    }
+}
