509b30f113b2b5b552e152e66510a1518058fc5a
Dan Kelly <dankelly989@gmail.com>
2018-01-04 17:24:34 +1300

    Upload some files from new version

---
+++ HosptaiL LM BS 23/Assets/Head Look Controller/Scripts/CursorHit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class CursorHit : MonoBehaviour {
+	
+	public HeadLookController headLook;
+	
+	// Update is called once per frame
+	void LateUpdate () {		
+		headLook.target = transform.position;
+	}
+}
