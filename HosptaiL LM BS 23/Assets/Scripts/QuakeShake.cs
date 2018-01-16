35ff7c2aa967aebb0c4ba348f9959a7fda93387e
Dan Kelly <dankelly989@gmail.com>
2018-01-17 12:51:17 +1300

    added a better broken wall and some new images. moved several file locations. changed the position of some objects

---
+++ HosptaiL LM BS 23/Assets/Scripts/QuakeShake.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuakeShake : MonoBehaviour
+{
+
+    public Transform camTransform;
+    public float shakeDuration = 3f;
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+    public bool active = false;
+
+    Vector3 originalPos;
+
+    void Start()
+    {
+        camTransform = GameObject.Find("RigidBodyFPSController").transform;
+    }
+
+    public void quake()
+    {
+        originalPos = camTransform.localPosition;
+        originalPos.y = 2.655005f;
+        active = true;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0 && active)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 3f;
+            active = false;
+        }
+    }
+}
