e1b8f11744d72df44ad061015db15b3b22588853
Dan Kelly <dankelly989@gmail.com>
2018-02-21 12:14:14 +1300

    stuff

---
+++ HosptaiL LM BS 23/Assets/endSequence.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endSequence : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Application.Quit();
+    }
+}
