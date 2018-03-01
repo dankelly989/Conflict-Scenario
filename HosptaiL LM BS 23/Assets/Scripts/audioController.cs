ba31bdbc8fbea217fc4797e5df3d793cac99bfc5
Dan Kelly <dankelly989@gmail.com>
2018-03-01 13:03:23 +1300

    mode some scripts and deleted some unused assets

---
+++ HosptaiL LM BS 23/Assets/Scripts/audioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
---
+++ HosptaiL LM BS 23/Assets/Scripts/clearQueue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clearQueue : MonoBehaviour {
+    subtitleController subtitles;
+
+    // Use this for initialization
+    void Start () {
+        subtitles = GameObject.Find("Subtitles").GetComponent<subtitleController>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        subtitles.stopDisplay();
+    }
+}
